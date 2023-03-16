f579ba68f55a20eccc4f6dc13c096e6ddeb2b537
LucasCrombet <lucascrombetstid@gmail.com>
2023-03-16 09:22:48 +0100

    update mices

---
+++ mices_protein.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import numpy as np
+import seaborn as sns
+from sklearn.datasets import load_digits
+
