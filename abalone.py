92b4e01dc3651bf59f37db8ab2cdcf75adfce5a5
LucasCrombet <lucascrombetstid@gmail.com>
2023-03-16 11:11:54 +0100

    kmeans = 6
    variable sex pas prise en compte

--- abalone.py
+++ abalone.py
@@ -9,3 +9,16 @@ import pandas as pd
 # Chargement du dataset
 column_names = ["Sex","Length","Diameter","Height","Whole weight","Shucked weight","Viscera weight","Shell weight","Rings"]
 abalone = pd.read_csv("abalone.data",names=column_names)
+
+data = abalone[["Length","Diameter","Height","Whole weight","Shucked weight","Viscera weight","Shell weight"]]
+y = abalone[["Rings"]]
+
+# Elbow Method pour les K means# Importation de ElbowVisualizer
+from yellowbrick.cluster import KElbowVisualizer
+model = KMeans()
+# k est l'intervalle du nombre de clusters.
+visualizer = KElbowVisualizer(model, k=(2,30), timings= True)
+visualizer.fit(data)       
+visualizer.show()        
+# D'après la Elbow Method, le nombre k de de clusters optimal est 6
+k = 6
