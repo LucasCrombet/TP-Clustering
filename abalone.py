d8a19265e2e9efbfabab4019eb07b91d8d110b6f
LucasCrombet <lucascrombetstid@gmail.com>
2023-03-23 14:17:58 +0100

    Fini
    J'ai bien refait les graphiques mais j'hésite entre 3 et 5 clusters.

--- abalone.py
+++ abalone.py
@@ -33,7 +33,12 @@ kmean= KMeans(5)
 kmean.fit(X)
 labels=kmean.labels_
 
+clusters=pd.concat([abalone, pd.DataFrame({'cluster':labels})], axis=1)
+clusters.head()
 
+for c in clusters:
+    grid= sns.FacetGrid(clusters, col='cluster')
+    grid.map(sns.histplot, c)
 
 dist = 1 - cosine_similarity(X)
 
@@ -52,11 +57,11 @@ colors = {0: 'red',
           3: 'yellow',
           4:"orange"}
 
-names = {0: 'Famille1', 
-         1: 'Famille2', 
-         2: 'Famille3', 
-         3: 'Famille4',
-         4: "Famille5"}
+names = {0: 'Principalement des enfants de petite taille et de poids très léger avec moins de 10 anneaux', 
+         1: 'Principalemnt des males et femelles de grande taille et de poids moyen avec entre 8 et 20 anneaux', 
+         2: 'Principalement des males et femelles de taille moyenne et de poids léger avec entre 8 et 20 anneaux', 
+         3: '3:Principalement des males et femelles de très grande taille et de poids lourd avec entre 8 et 20 anneaux',
+         4: "Principalement des enfants de taille moyenne et de poids léger avec entre 5 et 15 anneaux"}
   
 df = pd.DataFrame({'x': x, 'y':y, 'label':labels}) 
 groups = df.groupby('label')
@@ -71,5 +76,53 @@ for name, group in groups:
     ax.tick_params(axis= 'y',which='both',left='off',top='off',labelleft='off')
     
 ax.legend()
-ax.set_title("Customers Segmentation based on their Credit Card usage bhaviour.")
+ax.set_title("Clustering avec k=5 en fonction des caractéristiques")
+plt.show()
+
+###########################"
+
+kmean= KMeans(3)
+kmean.fit(X)
+labels=kmean.labels_
+
+clusters=pd.concat([abalone, pd.DataFrame({'cluster':labels})], axis=1)
+clusters.head()
+
+for c in clusters:
+    grid= sns.FacetGrid(clusters, col='cluster')
+    grid.map(sns.histplot, c)
+
+dist = 1 - cosine_similarity(X)
+
+pca = PCA(2)
+pca.fit(dist)
+X_PCA = pca.transform(dist)
+X_PCA.shape
+
+##############
+
+x, y = X_PCA[:, 0], X_PCA[:, 1]
+
+colors = {0: 'red',
+          1: 'blue',
+          2: 'green'}
+
+names = {0: 'Enfants de petite taille et de poids inférieur avec 10 anneaux ou moins', 
+         1: 'Males/Femelles de taille moyenne et de poids moyen avec entre 8 et 20 anneaux', 
+         2: 'Males/Femelles de grande taille et de poids supérieur avec entre 8 et 20 anneaux'}
+  
+df = pd.DataFrame({'x': x, 'y':y, 'label':labels}) 
+groups = df.groupby('label')
+
+fig, ax = plt.subplots(figsize=(20, 13)) 
+
+for name, group in groups:
+    ax.plot(group.x, group.y, marker='o', linestyle='', ms=5,
+            color=colors[name],label=names[name], mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(axis='x',which='both',bottom='off',top='off',labelbottom='off')
+    ax.tick_params(axis= 'y',which='both',left='off',top='off',labelleft='off')
+    
+ax.legend()
+ax.set_title("Clustering avec k=3 en fonction des caractéristiques")
 plt.show()
